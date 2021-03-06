69c82467443422215c8e46a6d6e4a14a6419a375
Wesley Bortolozo <wesleyjr0101@gmail.com>
2021-03-06 19:04:02 -0300

    starting ckd repo

--- turma4_cdk/turma4_cdk_stack.py
+++ turma4_cdk/turma4_cdk_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core as cdk
+from aws_cdk import aws_s3 as s3
 
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
@@ -8,8 +9,8 @@ from aws_cdk import core
 
 
 class Turma4CdkStack(cdk.Stack):
-
+    bucket_name = 'bucket-belisco-cdk-turma444'
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        s3.Bucket(self, id=self.bucket_name, bucket_name=self.bucket_name)
         # The code that defines your stack goes here
